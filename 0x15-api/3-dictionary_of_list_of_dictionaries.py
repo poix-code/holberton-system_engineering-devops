4b57e6189f3ce194ac8a616061aaba088a211714
poix-code <1996@holbertonschool.com>
2021-02-02 03:49:02 +0000

    List of list

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Get a lis of list of all employees"""
+if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
+    req = requests.get('https://jsonplaceholder.typicode.com/users/')
+    req = req.json()
+    ids = {}
+    for user in req:
+        ids[user.get("id")] = user.get("username")
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    dic = {}
+    for task in todos:
+        user_id = task.get("userId")
+        username = ids[user_id]
+        t = task.get("completed")
+        d = {"username": username, "task": t, "completed": completed}
+        if dic.get(user_id):
+            dic.get(user_id).append(d)
+        else:
+            dic[user_id] = [d]
+    with open('todo_all_employees.json', 'w', newline='') as f:
+        json.dump(task_dic, f)
